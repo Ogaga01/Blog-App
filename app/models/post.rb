e998e7b1a48a1845e657ad926bf629ad074d7072
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-05-01 15:01:58 +0100

    Modify Posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,13 @@
-class Like < ApplicationRecord
+class Post < ApplicationRecord
     belongs_to :user
-    belongs_to :post
+    has_many :comments
+    has_many :likes
   
-    def update_posts_likes_counter
-      post.increment!(:likes_counter)
+    def update_users_posts_counter
+      user.increment!(:post_counter)
+    end
+  
+    def most_recent_comments
+      comments.order(created_at: :desc).limit(5)
     end
   end
