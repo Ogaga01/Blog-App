1b3a06736061c9aa45c8bad191fdb3f876f55f37
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-05-09 08:14:53 +0100

    Add Current User

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,70 +1,26 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[ show edit update destroy ]
-
-  # GET /comments or /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1 or /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
   def new
-    @comment = Comment.new
+    @user = User.find_by(id: params[:user_id])
+    @post = Post.find_by(id: params[:post_id])
+    @comment = @post.comments.new
   end
 
-  # GET /comments/1/edit
-  def edit
-  end
-
-  # POST /comments or /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @user = current_user
+    @post = Post.find_by(id: params[:post_id])
+    @comment = Comment.new(post_id: params[:post_id], user_id: current_user.id, text: comment_params[:text])
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /comments/1 or /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully updated." }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comments/1 or /comments/1.json
-  def destroy
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: "Comment was successfully destroyed." }
-      format.json { head :no_content }
+    if @comment.save
+      @comment.update_posts_comments_counter
+      redirect_to user_post_path(@user.id, @post.id), notice: 'Comment created successfully!'
+    else
+      redirect_to user_post_path(@user.id, @post.id), notice: 'Sorry! something went wrong!'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def comment_params
-      params.require(:comment).permit(:text)
-    end
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
